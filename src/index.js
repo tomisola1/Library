55851a72b5e2e53f0915c266a90cba2c70e6ef63
tomisola1 <tomisoladebby@gmail.com>
2023-03-12 23:33:58 +0100

    feat: Auth setup

--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { Auth0Provider } from "@auth0/auth0-react";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Auth0Provider
+      domain="process.env.REACT_APP_DOMAIN"
+      clientId="process.env.REACT_APP_CLIENT_ID"
+    >
+      <App />
+    </Auth0Provider>
   </React.StrictMode>
 );
 
